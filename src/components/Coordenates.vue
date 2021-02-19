f9bae265c276016bb18ddcc13e49837c87396543
Tecnodix2020 <tecnodix2020@gmail.com>
2021-02-19 17:00:09 -0300

    Coleta da geolocalização funcionando - precisa que o usuário permita o uso no browser

--- src/components/Coordenates.vue
+++ src/components/Coordenates.vue
@@ -1,9 +1,74 @@
 <template>
-    <p>hello world!</p>
+    <q-page>
+        <div class="row justify-center q-mt-lg">
+            <q-btn
+                class="col-4"
+                color="primary"
+                label="Coletar Localização"
+                push
+                size="lg"
+                @click="getGeolocation"
+            />
+        </div>
+        <div class="row justify-center" >
+            <div class="col-10">
+                <p>
+                    Latitude: {{ this.latitude }}
+                </p>
+                <p>
+                    Longitude: {{ this.longitude }}
+                </p>
+            </div>
+        </div>
+    </q-page>
 </template>
 
 <script>
 export default {
-  name: 'Coordenates'
+  name: 'Coordenates',
+  data () {
+    return {
+      latitude: '',
+      longitude: ''
+    }
+  },
+  methods: {
+    getGeolocation () {
+      if (navigator.geolocation) {
+        this.$q.loading.show()
+        navigator.geolocation.getCurrentPosition(this.setPosition, this.errorPosition)
+      } else {
+        this.errorPosition()
+      }
+    },
+    setPosition (position) {
+      const coords = position.coords
+      this.latitude = coords.latitude
+      this.longitude = coords.longitude
+      this.$q.loading.hide()
+      this.successNotify()
+    },
+    errorPosition () {
+      this.$q.notify({
+        position: 'bottom',
+        timeout: 3000,
+        color: 'negative',
+        textColor: 'white',
+        actions: [{ icon: 'close', color: 'white' }],
+        message: 'Erro na coleta de coordenadas - Ative a Coleta do Browser'
+      })
+      this.$q.loading.hide()
+    },
+    successNotify () {
+      this.$q.notify({
+        position: 'bottom',
+        timeout: 3000,
+        color: 'positive',
+        textColor: 'white',
+        actions: [{ icon: 'check', color: 'white' }],
+        message: 'Coordenadas coletadas com sucesso!'
+      })
+    }
+  }
 }
 </script>
